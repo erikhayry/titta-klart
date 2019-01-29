756e3f7d3528bd6d52e04c0326f7891603e8fd59
Erik Portin <erik.portin@svt.se>
2019-01-29 12:19:23 +0100

    icon

--- background-script.js
+++ background-script.js
@@ -5,12 +5,10 @@ Then display a notification. The notification contains the URL,
 which we read from the message.
 */
 function notify(message) {
-    console.log('background script received message');
-
     if(typeof browser !== 'undefined'){
         browser.notifications.create({
             'type': 'basic',
-            'iconUrl': browser.extension.getURL('icons/link-48.png'),
+            'iconUrl': browser.extension.getURL('icons/logo@2x.png'),
             'title': 'Titta klart',
             'message':  message.daysLeft
         });
@@ -18,7 +16,7 @@ function notify(message) {
     } else if(typeof chrome !== 'undefined'){
         chrome.notifications.create('days-left', {
             'type': 'basic',
-            'iconUrl': 'icons/link-48.png',
+            'iconUrl': 'icons/logo@2x.png',
             'title': 'Titta klart',
             'message':  message.daysLeft
         }, function (id) {
